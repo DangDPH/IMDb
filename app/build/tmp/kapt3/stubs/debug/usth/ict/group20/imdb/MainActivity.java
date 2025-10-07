6c34a7fe6b5d0ff1d19ce8dd9c531fc78cb9c7ca
DangDPH <ddang11114816@gmail.com>
2025-10-07 22:43:25 +0700

    hehe

---
+++ app/build/tmp/kapt3/stubs/debug/usth/ict/group20/imdb/MainActivity.java
@@ -0,0 +1,62 @@
+package usth.ict.group20.imdb;
+
+@kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000B\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0004\n\u0002\u0010\u000e\n\u0002\b\u0006\n\u0002\u0010\b\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0007\u0018\u00002\u00020\u0001B\u0005\u00a2\u0006\u0002\u0010\u0002J\u0012\u0010\t\u001a\u00020\n2\b\u0010\u000b\u001a\u0004\u0018\u00010\fH\u0014J\b\u0010\r\u001a\u00020\nH\u0014J\b\u0010\u000e\u001a\u00020\nH\u0014J\u0010\u0010\u000f\u001a\u00020\n2\u0006\u0010\u0010\u001a\u00020\u0011H\u0002J\b\u0010\u0012\u001a\u00020\nH\u0002J\b\u0010\u0013\u001a\u00020\nH\u0002J\b\u0010\u0014\u001a\u00020\nH\u0002J\b\u0010\u0015\u001a\u00020\nH\u0002J\u001c\u0010\u0016\u001a\u00020\n2\u0006\u0010\u0017\u001a\u00020\u00182\n\u0010\u0019\u001a\u0006\u0012\u0002\b\u00030\u001aH\u0002J\b\u0010\u001b\u001a\u00020\nH\u0002J\b\u0010\u001c\u001a\u00020\nH\u0002J\b\u0010\u001d\u001a\u00020\nH\u0002J\u001c\u0010\u001e\u001a\u00020\n2\u0006\u0010\u0017\u001a\u00020\u00182\n\u0010\u0019\u001a\u0006\u0012\u0002\b\u00030\u001aH\u0002J\b\u0010\u001f\u001a\u00020\nH\u0002J\b\u0010 \u001a\u00020\nH\u0002R\u000e\u0010\u0003\u001a\u00020\u0004X\u0082\u0004\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0005\u001a\u00020\u0006X\u0082.\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0007\u001a\u00020\bX\u0082.\u00a2\u0006\u0002\n\u0000\u00a8\u0006!"}, d2 = {"Lusth/ict/group20/imdb/MainActivity;", "Landroidx/appcompat/app/AppCompatActivity;", "()V", "scrollHandler", "Landroid/os/Handler;", "scrollRunnable", "Ljava/lang/Runnable;", "viewPager", "Landroidx/viewpager2/widget/ViewPager2;", "onCreate", "", "savedInstanceState", "Landroid/os/Bundle;", "onPause", "onResume", "openUrl", "url", "", "setupBornToday", "setupExploreMoviesLists", "setupFeaturedCarousel", "setupFeaturedToday", "setupHorizontalRecyclerView", "recyclerViewId", "", "adapter", "Landroidx/recyclerview/widget/RecyclerView$Adapter;", "setupHotSearch", "setupMoreToExplore", "setupSearch", "setupVerticalRecyclerView", "setupWhatToWatchLists", "startAutoScroll", "app_debug"})
+public final class MainActivity extends androidx.appcompat.app.AppCompatActivity {
+    private androidx.viewpager2.widget.ViewPager2 viewPager;
+    @org.jetbrains.annotations.NotNull()
+    private final android.os.Handler scrollHandler = null;
+    private java.lang.Runnable scrollRunnable;
+    
+    public MainActivity() {
+        super();
+    }
+    
+    @java.lang.Override()
+    protected void onCreate(@org.jetbrains.annotations.Nullable()
+    android.os.Bundle savedInstanceState) {
+    }
+    
+    private final void setupFeaturedCarousel() {
+    }
+    
+    private final void startAutoScroll() {
+    }
+    
+    @java.lang.Override()
+    protected void onPause() {
+    }
+    
+    @java.lang.Override()
+    protected void onResume() {
+    }
+    
+    private final void setupSearch() {
+    }
+    
+    private final void setupHorizontalRecyclerView(int recyclerViewId, androidx.recyclerview.widget.RecyclerView.Adapter<?> adapter) {
+    }
+    
+    private final void setupVerticalRecyclerView(int recyclerViewId, androidx.recyclerview.widget.RecyclerView.Adapter<?> adapter) {
+    }
+    
+    private final void setupHotSearch() {
+    }
+    
+    private final void setupFeaturedToday() {
+    }
+    
+    private final void setupWhatToWatchLists() {
+    }
+    
+    private final void setupBornToday() {
+    }
+    
+    private final void setupExploreMoviesLists() {
+    }
+    
+    private final void setupMoreToExplore() {
+    }
+    
+    private final void openUrl(java.lang.String url) {
+    }
+}
